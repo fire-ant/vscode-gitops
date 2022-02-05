e20d71babc87eb795b8dfa219212c335f3514d8d
fire-ant <chris.lavery@weave.works>
2022-02-05 21:17:31 +0000

    add k3d and CAPI

--- kind/cluster_up.sh
+++ kind/cluster_up.sh
@@ -6,7 +6,7 @@ export CLUSTER_NAME=vscode-gitops
 
 # cluster_up:
 
-kind create cluster --config=./kind/kind.yml --name vscode-gitops
+kind create cluster --config=./kind/kind.yml --name $CLUSTER_NAME
 
 # replace_loopback:
 sed -i "s/127.0.0.1.*/$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CLUSTER_NAME-control-plane):6443/" ~/.kube/config 
