8b23bd4e9e06162ff236c06b2de705162cb9dc98
fire-ant <chris.lavery@weave.works>
2022-02-08 21:06:11 +0000

    add volume with manifests

--- k3d/cluster_up.sh
+++ k3d/cluster_up.sh
@@ -5,7 +5,7 @@ export CLUSTER_NAME=mgmt-lcl
 
 # cluster_up:
 
-k3d cluster create --config ./k3d/config.yaml # --name  
+k3d cluster create --config ./k3d/config.yaml --volume "$~MANIFESTS:/var/lib/rancher/k3s/server/manifests/"
 
 # replace_loopback:
 sed -i "s/0.0.0.0.*/$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' k3d-$CLUSTER_NAME-server-0):6443/" ~/.kube/config 
